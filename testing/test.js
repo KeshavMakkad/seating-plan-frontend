280e71272a126c6f55a6668e091fc9005feb9437
KeshavMakkad <keshav.23bcs10002@ms.sst.scaler.com>
2025-04-14 00:31:00 +0530

    fix 418 cache error

--- testing/test.js
+++ testing/test.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
-const TARGET_URL = "http://localhost:8000/data/T3%20--%20Group%201%20React%20Midterm%20--%20Test";
+// const TARGET_URL = "http://localhost:8000/data/T3%20--%20Group%201%20React%20Midterm%20--%20Test";
+// const TARGET_URL = "http://localhost:3000/data/T3%20--%20Group%201%20React%20Midterm%20--%20Test";
 // const TARGET_URL = "https://seating-paln-backend-1.onrender.com/data/T3%20--%20Group%201%20React%20Midterm%20--%20Test";
+//const TARGET_URL = "https://seating-backend.onrender.com/data/T3%20--%20Group%201%20React%20Midterm%20--%20Test";
+// const TARGET_URL = "https://seating-backend-1046236071390.us-central1.run.app/data/T3%20--%20Group%201%20React%20Midterm%20--%20Test";
+// const TARGET_URL = "http://34.100.135.136:8000/data/T3%20--%20Group%201%20React%20Midterm%20--%20Test";
+const TARGET_URL = "https://seating.works/data/T3%20--%20Group%201%20React%20Midterm%20--%20Test";
 
 async function makeRequest() {
     try {
